31a968e652bc4381a6ff988babebb4952f590551
satk0 <satko_a@op.pl>
2024-09-14 19:53:20 +0200

    Delete not needed things

--- flake.nix
+++ flake.nix
@@ -11,12 +11,7 @@
     let
       # Helper to provide system-specific attributes
       pkgs = import nixpkgs { inherit system; };
-      #fhs = pkgs.callPackage ./shell.nix {inherit pkgs; };
-
     in {
-        #devShells.default = fhs.env;
-        # TODO: Make it use FHSEnv as a sudo wrapper as polkit or run0 instead of docker
-        #devShells.default = pkgs.callPackage ./shell.nix {inherit pkgs; };
         devShells.default = pkgs.callPackage ./dev.nix {inherit pkgs; };
     }
   );
